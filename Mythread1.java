3d674f9b837714c7ea43366060b52c87eb2f492f
ThoratAditya <103573770+ThoratAditya@users.noreply.github.com>
2022-05-21 08:30:29 +0530

    Add files via upload

---
+++ Mythread1.java
@@ -0,0 +1,13 @@
+import java.lang.*;
+
+class Mythread1
+{
+  public static void main(String arg[])
+  {
+    System.out.println("Inside main");
+	
+	Thread tobj = Thread.currentThread();
+	
+	System.out.println("Name of current is :"+tobj.getName());
+  }
+}
